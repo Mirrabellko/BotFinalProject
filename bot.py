16761bb098a59e1720c6afe5f4f4134a4c99c21f
Mirrabellko <burlutskayavalery@mail.ru>
2024-07-27 12:09:21 +0300

    fix: closure added

--- bot.py
+++ bot.py
@@ -1,9 +1,44 @@
 import telebot
 
-from config import TOKEN
+from collections import namedtuple
 
+from config import TOKEN
+import database as db
 
 bot = telebot.TeleBot(TOKEN)
+
+Recipe = namedtuple('Recipe', ['title', 'category', 'ingredients', 'steps', 'cook_time'])
+User = namedtuple('User', ['username', 'password', 'email'])
+
+def user_data():
+
+    status = None
+    user = None
+
+    def closure(new_user: namedtuple):
+
+        user = new_user
+        print(f"user: {user}")
+
+        return closure
+
+def recipe_data():
+
+    user_recipe = None
+
+    def closure(recipe: namedtuple):
+
+        user_recipe = recipe
+        print(f"recipe: {user_recipe}")
+
+        return closure
+
+
+
+        
+        
+
+
 
 
 if __name__ == "__main__":
